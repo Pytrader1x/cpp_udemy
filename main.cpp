06d49dadc32a9a60379d1430bda2a813d2d7da72
pytrader1x <willukmail@gmail.com>
2022-01-24 21:26:45 +0000

    First commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+
+    int favourite_number;
+    std::cout << "Enter your favourtie number between 1 & 100" << std::endl;
+    std::cin >> favourite_number;
+    std::cout << "Your favourite number is:"<< favourite_number << std::endl;
+    return 0;
+
+}
