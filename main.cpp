0f54faa89fb08d2c1c0f262f9094488a3f0241ed
pytrader1x <willukmail@gmail.com>
2022-01-24 21:34:23 +0000

    final master push

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
-
+#include <climits>
+using namespace std;
 int main(){
 
     int favourite_number;
     std::cout << "Enter your favourtie number between 1 & 100" << std::endl;
     std::cin >> favourite_number;
     std::cout << "Your favourite number is:"<< favourite_number << std::endl;
+    std::cout << "SIze of fav number is: " << sizeof(favourite_number) << endl;
+    cout << "size of char " << sizeof(char) << endl;
     return 0;
 
 }
