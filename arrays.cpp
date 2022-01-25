09683bca12a9454d89d6ae8d6fb6e206fe35bb92
pytrader1x <willukmail@gmail.com>
2022-01-25 21:33:59 +0000

    update

---
+++ arrays.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int num {22};
+    // When declaring we must define the size of the array for the compiler
+    int test_scores [5] {100, 95, 99, 87, 88};
+
+    int high_score_per_level [10] {3, 5}; //init ele 0 & 1 == 3, 5
+
+    const int days_in_year {365};
+
+    double hi_temperatures [days_in_year] {0}; //init all to zero
+
+    int another_array [] {1,2,3,4,5}; // Size auto calculated
+    cout << days_in_year << endl;
+    
+    cout << "Which element in test_scores would you like to change" << endl;
+    int index = {0};
+    cin >> index;
+    cout << test_scores[index] << endl;
+}
--- loop.py
+++ loop.py
@@ -1,9 +1,12 @@
 from timeit import default_timer as timer
+from numba import jit
 
+@jit(nopython=True) # Set "nopython" mode for best performance, equivalent to @njit
 def addition(a, b):
 
     return a + b
 
+@jit(nopython=True) # Set "nopython" mode for best performance, equivalent to @njit
 def loop(num_iterations):
     
     for i in range(num_iterations):
@@ -11,6 +14,16 @@ def loop(num_iterations):
         total = addition(i, 3)
         # print(total)
 
+start = timer()
+loop(10000)
+print("{:.6f}".format(timer() - start))
+print('####')
+########
+start = timer()
+loop(10000)
+print("{:.6f}".format(timer() - start))
+print('####')
+#############
 start = timer()
 loop(10000)
 print("{:.6f}".format(timer() - start))
