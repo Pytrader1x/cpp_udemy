84e332c083503e0ab965180b50deb32b1b8a0bd0
pytrader1x <willukmail@gmail.com>
2022-01-31 12:17:05 +0000

    Updated loops

---
+++ multi_dimension_array.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    
+    int movie_rating [3][4]{
+
+        {2, 4, 5, 5},
+        {2, 4, 5, 2},
+        {2, 4, 3, 5},
+    };
+
+    cout << movie_rating[0][1] << endl;
+
+
+    
+}
