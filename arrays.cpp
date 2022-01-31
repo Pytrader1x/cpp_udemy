b9c9f054724ca74e6611a9bdf5ade541c6e24f58
pytrader1x <willukmail@gmail.com>
2022-01-31 12:09:41 +0000

    Updated loops

--- arrays.cpp
+++ arrays.cpp
@@ -8,6 +8,8 @@ int main(){
     // When declaring we must define the size of the array for the compiler
     int test_scores [5] {100, 95, 99, 87, 88};
 
+    int temperatures [5] {}; // This initialises everythin to zero
+
     int high_score_per_level [10] {3, 5}; //init ele 0 & 1 == 3, 5
 
     const int days_in_year {365};
@@ -17,8 +19,17 @@ int main(){
     int another_array [] {1,2,3,4,5}; // Size auto calculated
     cout << days_in_year << endl;
     
-    cout << "Which element in test_scores would you like to change" << endl;
+    cout << "Which element in test_scores would you like to change " << endl;
     int index = {0};
+    int value = {0};
     cin >> index;
     cout << test_scores[index] << endl;
+    cout << "What would you like to change 2" << test_scores[index] << " to?" << endl;
+
+    cin >> value;
+    test_scores[index] = value;
+    cout << "\nUpdated to " << test_scores[index] << endl;
+
+
+    
 }
