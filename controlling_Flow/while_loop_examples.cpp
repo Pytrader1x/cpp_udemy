b204e516e87a08544028629ed258c027e8da481e
pytrader1x <willukmail@gmail.com>
2022-02-15 21:33:40 +0000

    while

--- controlling_Flow/while_loop_examples.cpp
+++ controlling_Flow/while_loop_examples.cpp
@@ -5,6 +5,18 @@ using namespace std;
 
 int main(){ 
     //while loop
+
+    int num{};
+
+    cout << "Enter pos int to count upto:";
+    cin >> num;
+
+    int i{1};
+
+    while(num>=i){
+        cout << i << endl;
+        i++;
+    }
   
  
 
